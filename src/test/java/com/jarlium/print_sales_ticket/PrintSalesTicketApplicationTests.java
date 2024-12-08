355d48fe901224fcb49c5b19a590f8ebea050434
Frarm <frarm2014@outlook.com>
2024-12-08 09:31:03 -0500

    Inicio

---
+++ src/test/java/com/jarlium/print_sales_ticket/PrintSalesTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarlium.print_sales_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintSalesTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
