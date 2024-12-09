828d478aecbf9854130d7886315e49f7b3aaf239
Frarm <frarm2014@outlook.com>
2024-12-08 21:40:09 -0500

    Genera el documento correctamente con los comandos. Falta impresion

---
+++ README.md
@@ -0,0 +1,19 @@
+# Print Sales Ticket
+
+Este proyecto es una aplicación de Spring Boot que genera un ticket de ventas en formato DOCX utilizando Apache POI.
+
+## Requisitos
+
+- Java 17
+- Maven
+
+## Compilación
+
+Para compilar el proyecto, ejecuta el siguiente comando en el directorio raíz del proyecto:
+
+```bash
+mvn clean install
+java -jar target/print-sales-ticket-0.0.1-SNAPSHOT.jar "LOOP QUIET PLUS 2 BLACK" "Franco Romero" "Los Olivos"
+```
+
+
---
+++ src/main/java/com/jarlium/print_sales_ticket/PrintSalesTicketApplication.java
@@ -0,0 +1,63 @@
+package com.jarlium.print_sales_ticket;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.xwpf.usermodel.IRunBody;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlObject;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintSalesTicketApplication {
+
+	public static void main(String[] args) throws Exception {
+
+		XWPFDocument document = new XWPFDocument(new FileInputStream("src\\main\\resources\\TicketPlantilla.docx"));
+
+		String productName = args[0];
+		String clientNames = args[1];
+		String district = args[2];
+
+		for (XWPFParagraph paragraph : document.getParagraphs()) {
+			XmlCursor cursor = paragraph.getCTP().newCursor();
+			cursor.selectPath(
+					"declare namespace w='http://schemas.openxmlformats.org/wordprocessingml/2006/main' .//*/w:txbxContent/w:p/w:r");
+
+			List<XmlObject> ctrsintxtbx = new ArrayList<XmlObject>();
+
+			while (cursor.hasNextSelection()) {
+				cursor.toNextSelection();
+				XmlObject obj = cursor.getObject();
+				ctrsintxtbx.add(obj);
+			}
+			for (XmlObject obj : ctrsintxtbx) {
+				CTR ctr = CTR.Factory.parse(obj.xmlText());
+				XWPFRun bufferrun = new XWPFRun(ctr, (IRunBody) paragraph);
+				String text = bufferrun.getText(0);
+				if (text != null && text.contains("PRUEBA")) {
+					text = text.replace("PRUEBA", productName);
+					bufferrun.setText(text, 0);
+				} else if (text != null && text.contains("Client")) {
+					text = text.replace("Client", clientNames);
+					bufferrun.setText(text, 0);
+				} else if (text != null && text.contains("District")) {
+					text = text.replace("District", district);
+					bufferrun.setText(text, 0);
+				}
+				obj.set(bufferrun.getCTR());
+			}
+		}
+
+		FileOutputStream out = new FileOutputStream("src\\main\\resources\\Ticket.docx");
+		document.write(out);
+		out.close();
+		document.close();
+	}
+}
