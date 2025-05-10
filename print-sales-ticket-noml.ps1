9bae2e659246c7318e4d7647f42efb5af2b8fc23
Frarm <frarm2014@outlook.com>
2025-05-10 11:45:15 -0500

    Update PowerShell script parameters and remove outdated Outlook trigger

--- print-sales-ticket-noml.ps1
+++ print-sales-ticket-noml.ps1
@@ -1,23 +1,32 @@
 param (
-    [string]$arg1,
-    [string]$arg2,
-    [string]$arg3
+    [string]$actionType = "GP/NOGP",
+    [string]$customerName,
+    [string]$district,
+    [string]$address,
+    [string]$phone,
+    [string]$date,
+    [string]$productName,
+    [string]$observation
 )
 
 # Definir el path del JAR
-$jarPath = "D:\Java\print-sales-ticket-noml\target\print-sales-ticket-noml-0.0.1.jar"
-$jarArgs = "-jar $jarPath $arg1 $arg2 $arg3"
+$jarPathImprimir = "D:\Java\print-sales-ticket-noml\target\print-sales-ticket-noml-0.0.1.jar"
+$jarPathRegistrar = "D:\Java\register-gopack\target\register-gopack-0.0.1-SNAPSHOT.jar"
+$jarArgs = "$productName $customerName $district"
 
 # Mostrar los argumentos y el comando para depuración
-Write-Output "Ejecutando comando: java $jarArgs"
+Write-Output "Ejecutando comando: $jarArgs"
 
 # Ejecutar el programa Java que genera el documento Word con argumentos
-& java -jar $jarPath $arg1 $arg2 $arg3
+& java -jar $jarPathImprimir $productName $customerName $district
 
-# Pausa para evitar que la ventana se cierre inmediatamente 
-# Read-Host -Prompt "Presiona Enter para cerrar"
+# Si actionType es "Y", ejecutar un programa adicional
+if ($actionType -eq "GP") {
+    Write-Output "Registrando producto en gopack..."
+    & java -jar $jarPathRegistrar $customerName $district $address $phone $date $productName $observation
+}
 
-#Crear un objeto de aplicación de Word
+# Crear un objeto de aplicación de Word
 $word = New-Object -ComObject Word.Application
 $word.Visible = $false
 
